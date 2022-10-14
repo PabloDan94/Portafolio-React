11d5757937b3e2505a5eb59b297c05cef2de8f5d
Pablo <pablo.iriarte.94@hotmail.com>
2022-10-14 18:58:53 -0300

    agregando nuevo proyecto

--- src/Components/MisProyectos.jsx
+++ src/Components/MisProyectos.jsx
@@ -5,6 +5,7 @@ import Reactflix from '../Img/reactflix.png'
 import '../Styles/MisProyectos.css'
 import FondoProyect from '../Img/fondo.png.png'
 import FondoProyect2 from '../Img/proyecto3.png.png'
+import CafeStore from '../Img/CafeStore.png'
 
 
 export default function MisProyectos() {
@@ -14,6 +15,7 @@ export default function MisProyectos() {
             <h1 className="titulo__proyectos">Mis proyectos</h1>
             <Proyecto imagen={Portafolio} titulo="Portafolio" descripcion="Este es mi portafolio personal realizado con React.js"/>
             <Proyecto imagen={Reactflix} titulo="Reactflix" descripcion="Una pagina de peliculas realizada con React.js" link="https://reactmovies-pablodan.netlify.app"/>
+            <Proyecto imagen={CafeStore} titulo="Café Store" descripcion="Pagina de una tienda de café realizada con Html, Css y Javascript" link="https://reactmovies-pablodan.netlify.app"/>
             <img className="fondo__proyect" src={FondoProyect} alt="proyecto-img"></img>
         </div>
     )
